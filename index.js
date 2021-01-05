edb38a47b5608bf8e6927131b8467950c843606a
guifu.wu <502556093@qq.com>
2021-01-05 15:17:16 +0800

    feat: 兼容win系统

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const bodyParser = require('koa-bodyparser')
+const path = require('path')
+const chalk = require('chalk')
+const config = require('./lib/config')
 const editorRouter = require('./lib/router')
 const createMock = require('./lib/mock')
 const { injectComponents } = require('./lib/template')
@@ -11,4 +14,17 @@ module.exports = function(app) {
   const mockRouter = createMock()
   app.use(mockRouter.routes())
   app.use(mockRouter.allowedMethods())
+
+  const { name } = path.parse(process.cwd())
+  const route = config.editorPath.replace(config.pagesPath, '')
+    .split(path.sep).filter(item => !!item).join('/')
+
+  console.log()
+  console.log(chalk.yellow('[编辑器] 启动完成'))
+  console.log(chalk.yellow('[编辑器] 编辑器访问地址'))
+  console.log(chalk.yellow(`[编辑器] http://cabin.dmall.com?debug-${name}=http://local.dmall.com:3000/kayak-project#full/${name}/${route}`))
+  console.log()
+  console.log(chalk.yellow('[HOST] 请确保已经配置如下host'))
+  console.log(chalk.yellow('[HOST] local.dmall.com 127.0.0.1'))
+  console.log()
 }
