215dfa80a23d1e690d0326e1c74dcde895c2ce74
guifu.wu <502556093@qq.com>
2021-01-18 09:37:27 +0800

    feat: 结合cabinx

--- index.js
+++ index.js
@@ -6,25 +6,31 @@ const editorRouter = require('./lib/router')
 const createMock = require('./lib/mock')
 const { injectComponents } = require('./lib/template')
 
-module.exports = function(app) {
-  injectComponents()
-  app.use(bodyParser())
-  app.use(editorRouter.routes())
-  app.use(editorRouter.allowedMethods())
-  const mockRouter = createMock()
-  app.use(mockRouter.routes())
-  app.use(mockRouter.allowedMethods())
+const mockRouter = createMock()
 
-  const { name } = path.parse(process.cwd())
-  const route = config.editorPath.replace(config.pagesPath, '')
-    .split(path.sep).filter(item => !!item).join('/')
+module.exports = {
+  middleware: [
+    bodyParser(),
+    editorRouter.routes(),
+    editorRouter.allowedMethods(),
+    mockRouter.routes(),
+    mockRouter.allowedMethods(),
+  ],
+  start: function (callback) {
+    injectComponents()
+    const { name } = path.parse(process.cwd())
+    const route = config.editorPath.replace(config.pagesPath, '')
+      .split(path.sep).filter(item => !!item).join('/')
+  
+    callback()
 
-  console.log()
-  console.log(chalk.yellow('[编辑器] 启动完成'))
-  console.log(chalk.yellow('[编辑器] 编辑器访问地址'))
-  console.log(chalk.yellow(`[编辑器] http://cabin.dmall.com?debug-${name}=http://local.dmall.com:3000/kayak-project#full/${name}/${route}`))
-  console.log()
-  console.log(chalk.yellow('[HOST] 请确保已经配置如下host'))
-  console.log(chalk.yellow('[HOST] local.dmall.com 127.0.0.1'))
-  console.log()
+    console.log()
+    console.log(chalk.yellow('[编辑器] 启动完成'))
+    console.log(chalk.yellow('[编辑器] 编辑器访问地址'))
+    console.log(chalk.yellow(`[编辑器] http://cabin.dmall.com?debug-${name}=http://local.dmall.com:3000/kayak-project#full/${name}/${route}`))
+    console.log()
+    console.log(chalk.yellow('[HOST] 请确保已经配置如下host'))
+    console.log(chalk.yellow('[HOST] local.dmall.com 127.0.0.1'))
+    console.log()
+  }
 }
